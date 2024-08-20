76d23c3c47f5eab0a6fbf7a1ded129db69fdaab1
emad-desoky <deskoemad@gmail.com>
2024-08-20 13:39:16 +0300

    ..

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer/Footer";
 import { FramerMotion } from "@/components/framer-motion/FramerMotion";
 import Hero from "@/components/hero/Hero";
 import NavBar from "@/components/navbar/Navbar";
-import NewProducts from "@/components/newProducts/NewProducts";
+import NewProducts from "@/components/new-products/NewProducts";
 import Products from "@/components/products/Products";
 import Sales from "@/components/sales/Sales";
 
